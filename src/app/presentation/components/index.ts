9e4755b7b054c77eb3861d1f835925e3b16f6bf1
Agustín Cundari <agustin.cundari@grupoesfera.com.ar>
2024-05-06 09:12:14 -0300

    Add messages and text message box to Orthography Page

--- src/app/presentation/components/index.ts
+++ src/app/presentation/components/index.ts
@@ -1,2 +1,4 @@
 export * from './chat-bubbles/chatMesage/chatMesage.component'
 export * from './sidebarMenuItem/sidebarMenuItem.component';
+export * from './typingLoader/typingLoader.component';
+export * from './text-boxes/textMessageBox/textMessageBox.component'
--- src/app/presentation/pages/orthographyPage/orthographyPage.component.ts
+++ src/app/presentation/pages/orthographyPage/orthographyPage.component.ts
@@ -1,12 +1,21 @@
 import { CommonModule } from '@angular/common';
 import { ChangeDetectionStrategy, Component } from '@angular/core';
-import { ChatMesageComponent } from '@components/index';
+import { ChatMesageComponent, TextMessageBoxComponent, TypingLoaderComponent } from '@components/index';
 
 @Component({
   selector: 'app-orthography-page',
   standalone: true,
-  imports: [CommonModule, ChatMesageComponent],
+  imports: [
+    CommonModule,
+    ChatMesageComponent,
+    TypingLoaderComponent,
+    TextMessageBoxComponent,
+  ],
   templateUrl: './orthographyPage.component.html',
   changeDetection: ChangeDetectionStrategy.OnPush,
 })
-export default class OrthographyPageComponent {}
+export default class OrthographyPageComponent {
+  handleMessage(prompt: string): void {
+
+  }
+}
