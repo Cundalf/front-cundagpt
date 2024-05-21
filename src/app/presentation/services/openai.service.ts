dc8e8c236ab2971386aa626ff52973c601f69f46
Agustín Cundari <agustin.cundari@grupoesfera.com.ar>
2024-05-21 00:10:27 -0300

    Add Translate feature

--- src/app/presentation/services/openai.service.ts
+++ src/app/presentation/services/openai.service.ts
@@ -1,5 +1,5 @@
 import { Injectable } from '@angular/core';
-import { orthographyUseCase, prosConsStreamUseCase, prosConsUseCase } from '@use-cases/index';
+import { orthographyUseCase, prosConsStreamUseCase, prosConsUseCase, translateTextUseCase } from '@use-cases/index';
 
 import { from } from 'rxjs';
 
@@ -15,5 +15,9 @@ export class OpenAiService {
 
   prosConsStreamDiscusser(prompt: string, abortSignal: AbortSignal) {
     return prosConsStreamUseCase(prompt, abortSignal);
+  }
+
+  translateText(prompt: string, lang: string) {
+    return from(translateTextUseCase(prompt, lang));
   }
 }
